49e57d9e377fea984ba8316cc31dd5476e109093
Timo Schwind <81616756+timosch11@users.noreply.github.com>
2022-10-30 15:45:15 +0100

    Running version

---
+++ lib/pages/loginscreen.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:chat_bubbles/chat_bubbles.dart';
+import 'registerscreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class MyLoginWidget extends StatefulWidget {
+  const MyLoginWidget({super.key});
+
+  @override
+  MyLoginWidgetState createState() => MyLoginWidgetState();
+}
+
+class MyLoginWidgetState extends State<MyLoginWidget> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose(){
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image(
+                  image: AssetImage('assets/images/logo.png'),
+                  width: 144,
+                  height: 185),
+              BubbleSpecialThree(
+                text: 'Hi!',
+                color: Color(0xffA1CAD0),
+                tail: true,
+                isSender: false,
+                textStyle: TextStyle(color: Colors.white, fontSize: 25),
+              ),
+            ],
+          ),
+          RichText(
+              text: TextSpan(
+                  text: "Welcome 👋",
+                  style: TextStyle(
+                      fontSize: 40,
+                      color: Color(0xffA1CAD0),
+                      fontWeight: FontWeight.bold))),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30),
+            child: Form(
+              child: Column(children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: emailController,
+                    decoration: InputDecoration(
+                        labelText: "E-Mail",
+                        hintText: "Enter your e-mail Adress",
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder()),
+                    onChanged: (String value) {},
+                    validator: (value) {
+                      return value!.isEmpty
+                          ? "Bitte wähle eine E-Mail Adresse"
+                          : null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: TextFormField(
+                    obscureText: true,
+                    obscuringCharacter: "*",
+                    controller: passwordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                        labelText: "Password",
+                        hintText: "Enter your Password",
+                        prefixIcon: Icon(Icons.password),
+                        border: OutlineInputBorder()),
+                    onChanged: (String value) {},
+                    validator: (value) {
+                      return value!.isEmpty ? "Bitte wähle ein Passwort" : null;
+                    },
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0, right: 50),
+                      child: MaterialButton(
+                        onPressed: () {
+                          signIn();
+                        },
+                        child: Text(
+                          "Login",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        height: 70,
+                        minWidth: 150,
+                        color: Color(0xffA1CAD0),
+                        textColor: Colors.white,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: MaterialButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => MyRegWidget()));
+                        },
+                        child: Text(
+                          "Register",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        height: 70,
+                        minWidth: 150,
+                        color: Color(0xffA1CAD0),
+                        textColor: Colors.white,
+                      ),
+                    ),
+                  ],
+                )
+              ]),
+            ),
+          )
+        ])));
+  }
+  Future signIn() async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(email: emailController.text.trim()
+    , password: passwordController.text.trim())
+  }
+}
---
+++ lib/pages/registerscreen.dart
@@ -0,0 +1,223 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:chat_bubbles/chat_bubbles.dart';
+import 'package:age_calculator/age_calculator.dart';
+
+class MyRegWidget extends StatefulWidget {
+  const MyRegWidget({super.key});
+
+  @override
+  _MyRegisterWidgetState createState() => _MyRegisterWidgetState();
+}
+
+class _MyRegisterWidgetState extends State<MyRegWidget> {
+  bool isChecked = false;
+  DateTime date = DateTime(2022, 12, 24);
+  String textfordate = "Geburtstag";
+  late DatabaseReference dbRef;
+  final emailController = TextEditingController();
+  final usernameController = TextEditingController();
+  final birthdayController = TextEditingController();
+  final passwordController = TextEditingController();
+  void initState() {
+    super.initState();
+    dbRef = FirebaseDatabase.instance.ref().child("Student");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isKeyboard = MediaQuery.of(context).viewInsets.bottom != 0;
+    return MaterialApp(
+        home: Scaffold(
+            resizeToAvoidBottomInset: false,
+            body: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      if (!isKeyboard)
+                        Image(
+                            image: AssetImage('assets/images/logo.png'),
+                            width: 144,
+                            height: 185),
+                      if (!isKeyboard)
+                        BubbleSpecialThree(
+                          text: 'Schön dich \nkennen zu \nlernen',
+                          color: Color(0xffA1CAD0),
+                          tail: true,
+                          isSender: false,
+                          textStyle:
+                              TextStyle(color: Colors.white, fontSize: 25),
+                        ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
+                    child: TextFormField(
+                      controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          labelText: "E-Mail",
+                          hintText: "Gebe hier deine E-Mail Adresse an",
+                          prefixIcon: Icon(Icons.email),
+                          border: OutlineInputBorder()),
+                      onChanged: (String value) {},
+                      validator: (value) {
+                        return value!.isEmpty
+                            ? "Bitte gebe hier deine e-mail Adresse an"
+                            : null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    child: TextFormField(
+                      controller: usernameController,
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                          labelText: "Nutzername",
+                          hintText: "Gebe hier deinen Nutzernamen an",
+                          prefixIcon: Icon(Icons.email),
+                          border: OutlineInputBorder()),
+                      onChanged: (String value) {},
+                      validator: (value) {
+                        return value!.isEmpty
+                            ? "Bitte gebe hier deinen Nutzernamen an"
+                            : null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: TextFormField(
+                      controller: birthdayController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          labelText: textfordate,
+                          hintText: "Gebe hier dein Geburtsdatum an",
+                          prefixIcon: Icon(Icons.date_range),
+                          border: OutlineInputBorder()),
+                      onChanged: (String value) {},
+                      onTap: () async {
+                        FocusScope.of(context).requestFocus(new FocusNode());
+                        DateTime? newDate = await showDatePicker(
+                          context: context,
+                          initialDate: date,
+                          firstDate: DateTime(1920),
+                          lastDate: DateTime(2025),
+                          builder: (context, child) {
+                            return Theme(
+                              data: Theme.of(context).copyWith(
+                                colorScheme: ColorScheme.light(
+                                  primary: Color(
+                                      0xffA1CAD0), // header background color
+                                  onPrimary: Colors.black, // header text color
+                                  onSurface: Colors.green, // body text color
+                                ),
+                                textButtonTheme: TextButtonThemeData(
+                                  style: TextButton.styleFrom(
+                                    primary:
+                                        Color(0xffA1CAD0), // button text color
+                                  ),
+                                ),
+                              ),
+                              child: child!,
+                            );
+                          },
+                        );
+
+                        if (newDate == null) return;
+                        setState(() {
+                          date = newDate;
+                          textfordate =
+                              "${date.day} - ${date.month} - ${date.year}";
+                        });
+                      },
+                      validator: (value) {
+                        return value!.isEmpty
+                            ? "Bitte gebe hier deine e-mail Adresse an"
+                            : null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    child: TextFormField(
+                      controller: passwordController,
+                      obscureText: true,
+                      obscuringCharacter: "*",
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                          labelText: "Password",
+                          hintText: "Nun musst du nurnoch ein Passwort wählen",
+                          prefixIcon: Icon(Icons.password),
+                          border: OutlineInputBorder()),
+                      onChanged: (String value) {},
+                      validator: (value) {
+                        return value!.isEmpty
+                            ? "Bitte wähle ein Passwort"
+                            : null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    child: TextFormField(
+                      obscureText: true,
+                      obscuringCharacter: "*",
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                          labelText: "Password",
+                          hintText: "und es einmal bestätigen",
+                          prefixIcon: Icon(Icons.password),
+                          border: OutlineInputBorder()),
+                      onChanged: (String value) {},
+                      validator: (value) {
+                        return value!.isEmpty
+                            ? "Bitte bestätige dein Passwort"
+                            : null;
+                      },
+                    ),
+                  ),
+                  CheckboxListTile(
+                    title: const Text(
+                        "Ich habe die AGB's gelesen und bin mindestens 8 Jahre alt"),
+                    onChanged: (value) {
+                      setState(() {
+                        isChecked = value!;
+                      });
+                    },
+                    value: isChecked,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: MaterialButton(
+                      onPressed: () {
+                        Map<String, String> Student = {
+                          "email": emailController.text,
+                          "username": usernameController.text,
+                          "birthday": date.toString(),
+                          "age": AgeCalculator.age(date).years.toString(),
+                          "password": passwordController.text
+                        };
+                        dbRef.push().set(Student);
+                      },
+                      child: Text(
+                        "Registrieren!",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      height: 70,
+                      minWidth: 200,
+                      color: Color(0xffA1CAD0),
+                      textColor: Colors.white,
+                    ),
+                  ),
+                ])));
+  }
+}
